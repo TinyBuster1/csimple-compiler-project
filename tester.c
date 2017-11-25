6ab53d98020ae4afcfc3830bae9667518eee402e
dorkk0 <dor07021991@@gmail.com>
2017-11-25 19:53:04 +0200

    0.4

--- tester.c
+++ tester.c
@@ -1,14 +1,22 @@
 void Main()
 {
-	int j, k, m, n;
-    boolean b;
-
+        string foo( boolean x )
+        {
+            m = m + inc;
+		    return true;
+        }
+	
+	int j, k, m, n; 
+	string[19] x;
+	char b;
 
 	j = 2;
 	k = - j;
 	m = 100 - k * k;
-    b=true;
-
-
-	return 0;
+	x[1] = foo( 1 );
+	n = printint( m );
+	x = "this is a s"tring['5']\n";
+	x = "this is \"; x = "invalid";
+	x[1] = 'h';
+    return x[7+1];
 }
