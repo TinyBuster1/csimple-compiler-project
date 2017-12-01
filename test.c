3209aed4687ac12092b204116e3c04dd9e2a53a4
Aryeh Katz <in@akatz.io>
2017-12-01 18:38:30 +0200

    test 1/2

--- test.c
+++ test.c
@@ -6,3 +6,19 @@ int main()
     x = x > 1;
     return 1;
 }
+int main(int x)
+{
+    if (x > 4 && a > 1 || a < 0)
+        asd = sad;
+
+    x = x > 1;
+    return 1;
+}
+int main(int y, char x)
+{
+    if (x > 4 && a > 1 || a < 0)
+        asd = sad;
+
+    x = x > 1;
+    return 1;
+}
